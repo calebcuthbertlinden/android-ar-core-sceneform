852d21dde53539b32035fc7336cfe56069743ba1
Caleb Linden <calebl@discovery.co.za>
2020-11-30 22:24:25 +0200

    First AR app woop woop

---
+++ app/src/main/java/com/linden/first_ar_application/MainActivity.java
@@ -0,0 +1,52 @@
+package com.linden.first_ar_application;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.ar.core.Anchor;
+import com.google.ar.sceneform.AnchorNode;
+import com.google.ar.sceneform.rendering.ModelRenderable;
+import com.google.ar.sceneform.ux.ArFragment;
+import com.google.ar.sceneform.ux.TransformableNode;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ArFragment arFragment;
+    private ModelRenderable modelRenderable;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        arFragment = (ArFragment) getSupportFragmentManager().findFragmentById(R.id.ar_fragment);
+        setUpModel();
+        setUpPlane();
+    }
+
+    private void setUpModel() {
+        ModelRenderable.builder().setSource(this, R.raw.sceneform_footprint).build().thenAccept(renderable -> modelRenderable = renderable)
+                .exceptionally(throwable -> {
+                    Toast.makeText(MainActivity.this, "", Toast.LENGTH_LONG).show();
+                    return null;
+                });
+    }
+
+    private void setUpPlane() {
+        arFragment.setOnTapArPlaneListener((hitResult, plane, motionEvent) -> {
+            Anchor anchor = hitResult.createAnchor();
+            AnchorNode anchorNode = new AnchorNode(anchor);
+            anchorNode.setParent(arFragment.getArSceneView().getScene());
+            createModel(anchorNode);
+        });
+    }
+
+    private void createModel(AnchorNode anchorNode) {
+        TransformableNode node = new TransformableNode((arFragment.getTransformationSystem()));
+        node.setParent(anchorNode);
+        node.setRenderable(modelRenderable);
+        node.select();
+    }
+}
